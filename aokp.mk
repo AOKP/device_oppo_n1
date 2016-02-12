58541a8a58068f2d38d9c77129647592db2063d2
Dirk Rettschlag <dirk.rettschlag@gmail.com>
2016-02-12 14:00:21 +0100

    AOKP-fy
    Change-Id: Ib817f199ca8cbc025c6974bda2fbb839841b780b
    Signed-off-by: Dirk Rettschlag <dirk.rettschlag@gmail.com>

--- aokp.mk
+++ aokp.mk
@@ -1,10 +1,10 @@
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aokp/configs/common_full_phone.mk)
 
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/aokp/configs/nfc_enhanced.mk)
 
 $(call inherit-product, device/oppo/n1/full_n1.mk)
 
-PRODUCT_NAME := cm_n1
+PRODUCT_NAME := aokp_n1
 PRODUCT_DEVICE := n1
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
